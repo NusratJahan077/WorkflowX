6ef6da71b311315963b5afed07cbcfc5f1d81c1f
NusratJahan077 <narifa872@gmail.com>
2026-04-09 00:46:20 +0600

    Update code

--- src/Component/Models/Models.jsx
+++ src/Component/Models/Models.jsx
@@ -10,19 +10,7 @@ const Models = ({ dataPromise  ,carts,setCarts}) => {
   return (
     <div className='px-4 md:px-10 lg:px-20'>
       
-      {/* Header Section */}
-      <div className='text-center py-10 md:py-20'>
-        <h2 className='text-3xl md:text-5xl font-extrabold'>
-          Premium Digital Tools
-        </h2>
-
-        <p className='text-sm md:text-base mt-3'>
-          Choose from our curated collection of premium digital products designed
-          <br className='hidden md:block' />
-          to boost your productivity and creativity.
-        </p>
-</div>
-  
+     
 
 
       {/* Cards Section */}
